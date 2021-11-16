b148732ee398dee60cd2e49e08bcf14d27fdea61
RubensRafael <rubensrafael2@live.com>
2021-11-16 17:20:29 -0200

    setup do MongoDB feito, criação e login concluídos

--- graphql-schemas/resolvers.js
+++ graphql-schemas/resolvers.js
@@ -1,7 +1,53 @@
+import dotenv from 'dotenv';
+import connect from '../database.js';
+import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
+
+dotenv.config()
+
+
+
+let db = await connect().then((mongodb)=>{
+  return mongodb
+})
+
+let users = db.collection("Users")
+
 // The root provides a resolver function for each API endpoint
 var resolvers = {
-  hello: () => {
-    return 'Hello world!';
+  createUser: async ({name, email, password}) => {
+
+    let cursor = await users.find({email : String(email)})
+    let list = await cursor.toArray()
+    if(list.length > 0){throw Error("this email already exits")}
+
+    let newUser = await bcrypt.hash(String(password), 10).then(async (hash)=>{
+      let result = await users.insertOne({name:String(name),email:String(email),password:String(hash)})
+      let user = await users.findOne({_id:result.insertedId})
+      return user
+    })
+    
+    let jwtToken = jwt.sign({id : newUser._id}, process.env.JWTKEY, {expiresIn: "3 days"})
+    await cursor.close()
+    return jwtToken
+   
   },
+  loginUser : async ({email, password}) =>{
+
+    let user = await users.findOne({email : String(email)}, {_id : 1, password : 1})
+    
+    if(user === null){throw Error("The email or the password is wrong.")}
+    await bcrypt.compare(password, user.password).then((result)=>{
+      if(result === false){console.log('a');throw Error("The email or the password is wrong.")}
+    })
+    let jwtToken = jwt.sign({id : user._id}, process.env.JWTKEY, {expiresIn: "3 days"})
+    return jwtToken
+
+    
+  }
 };
 export default resolvers
+/*jwt.verify(token, 'wrong-secret', function(err, decoded) {
+  // err 
+  // decoded undefined
+});*/
--- graphql-schemas/schema.js
+++ graphql-schemas/schema.js
@@ -2,8 +2,20 @@ import { buildSchema } from 'graphql';
 
 // Construct a schema, using GraphQL schema language
 var schema = buildSchema(`
+  type User {
+    _id : ID!
+    name : String!
+    password: String!
+    email : String!
+
+  }
+
+  type Mutation {
+    createUser(name: String, email: String, password: String) : String!
+  }
+
   type Query {
-    hello: String
+    loginUser(email: String, password: String) : String!
   }
 `);
 
